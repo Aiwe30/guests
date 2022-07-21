fdeeafaa6d1c273c8d8e63f89cf47298d483f466
Aivaras1234 <aivarasbankauskas@gmail.com>
2022-07-21 19:26:13 +0300

    Added HomePage Component, routes.js, package-lock.json Updated App.vue, main.js, style.css, index.html, package.json

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { createPinia, PiniaVuePlugin } from "pinia";
+import { createRouter, createWebHistory} from "vue-router";
+import { routes } from './routes.js';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const vm = createApp(App);
+vm.use(pinia);
+vm.use(router);
+vm.use(PiniaVuePlugin);
+vm.mount('#app');
+
