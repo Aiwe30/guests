cb8f5f03d175ee9faab9d59ddfd978343a23f331
Aivaras1234 <aivarasbankauskas@gmail.com>
2022-07-21 19:53:44 +0300

    Added Header, SideBar, Footer Components Updated App.vue, HomePage.vue routes.js

---
+++ src/views/layout/FooterLayot.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary fixed-bottom">
+    <div class="container-fluid d-flex justify-content-center">
+      <small class="text-light py-2">&copy; TeamBuild.Inc 2022</small>
+    </div>
+  </nav>
+</template>
---
+++ src/views/layout/HeaderLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-brand">TeamBuild</div>
+    </div>
+  </nav>
+</template>
