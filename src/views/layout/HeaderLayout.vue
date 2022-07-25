33bc263e0063b02922b75c06f7695820994e1457
Aivaras1234 <aivarasbankauskas@gmail.com>
2022-07-25 06:12:25 +0300

    Last Last Touch

--- src/views/layout/HeaderLayout.vue
+++ src/views/layout/HeaderLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-dark bg-primary fixed-top">
     <div class="container-fluid">
-      <div class="navbar-brand">TeamBuild</div>
+      <div class="navbar-brand">CityGuest</div>
     </div>
   </nav>
 </template>
