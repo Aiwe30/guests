bccbd38bd1b68280b18ac014a642f1717e4f45b9
Aivaras1234 <aivarasbankauskas@gmail.com>
2022-07-25 05:06:35 +0300

    Last Touches

--- src/views/pages/CityPage.vue
+++ src/views/pages/CityPage.vue
@@ -16,7 +16,7 @@ const city = cityStore.getCitiesWithGuests.find(city => city.id === Number(route
       <div class="card-title text-center mt-3">
         <h5>{{ city.cityName }}</h5>
       </div>
-      <div class="card-body">
+      <div class="card-body" v-if="city.cityGuests.length > 0">
         <div class="card border-0 shadow my-2" v-for="guest in city.cityGuests" :key="guest.id">
           <div class="row card-body">
             <div class="col-md-2 d-flex justify-content-center align-content-center">
@@ -30,6 +30,20 @@ const city = cityStore.getCitiesWithGuests.find(city => city.id === Number(route
             </div>
             <div class="col-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center align-items-center">
               <a :href = "'mailto:' + guest.email">contact me by email</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card-body" v-else>
+        <div class="card border-0 shadow my-2" >
+          <div class="row card-body">
+            <div class="col-12 text-center">
+              <div class="py-3">
+                <router-link to="/create-guest" class="text-decoration-none">
+                  <i class="bi bi-plus-circle-dotted me-2"></i>
+                  Add Guest To The City
+                </router-link>
+              </div>
             </div>
           </div>
         </div>
